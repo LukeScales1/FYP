779588350f5f9ef712bcdb3632a29924c17e22a0
luke_linux <luke.scales2@gmail.com>
2018-04-03 14:34:04 +0100

    Update for classifier, classifies apples, bananas, oranges, bell peppers, and tomatoes

--- app/src/main/java/com/example/luke/fyp/classifier/ImageClassifier.java
+++ app/src/main/java/com/example/luke/fyp/classifier/ImageClassifier.java
@@ -45,10 +45,10 @@ public class ImageClassifier {
   private static final String TAG = "TfLiteCameraDemo";
 
   /** Name of the model file stored in Assets. */
-  private static final String MODEL_PATH = "graph.lite";
+  private static final String MODEL_PATH = "retrained_graph.pb";
 
   /** Name of the label file stored in Assets. */
-  private static final String LABEL_PATH = "labels.txt";
+  private static final String LABEL_PATH = "retrained_labels.txt";
 
   /** Number of results to show in the UI. */
   private static final int RESULTS_TO_SHOW = 1;
